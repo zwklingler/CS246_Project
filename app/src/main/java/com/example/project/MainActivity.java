da7943b42d0c6ec80888daa10bffc285f831138b
Zachary Klingler <zwklingler26@byui.edu>
2019-02-22 10:59:44 -0700

    Added Basic Main Activity and Google Maps

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openMaps(View view) {
+        Log.d("Intent Debug: ","Starting Maps Activity");
+        //Create Intent
+        Intent intent = new Intent(this, Maps.class);
+
+        startActivity(intent);
     }
 }
